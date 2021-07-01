aceccf25001ba61e89ef61c71867e1c4b3752dc9
feiqn <feiqn.k@gmail.com>
2021-07-01 16:07:10 -0700

    smfh

---
+++ desktop/src/com/feiqn/wyrm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.feiqn.wyrm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.feiqn.wyrm.WYRMGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WYRMGame(), config);
+	}
+}
