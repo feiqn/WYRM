f563d43b4c0085cb88863b419e5788d0781e2d09
feiqn <feiqn.k@gmail.com>
2022-12-02 10:44:57 -0800

    hhh

---
+++ core/src/com/feiqn/wyrm/logic/screens/gamescreens/BattleScreen_1A.java
@@ -0,0 +1,11 @@
+package com.feiqn.wyrm.logic.screens.gamescreens;
+
+import com.feiqn.wyrm.WYRMGame;
+import com.feiqn.wyrm.logic.screens.BattleScreen;
+
+public class BattleScreen_1A extends BattleScreen {
+
+    public BattleScreen_1A(WYRMGame game) {
+        super(game);
+    }
+}
--- core/src/com/feiqn/wyrm/models/mapdata/prefabLogicalMaps/stage_1a.java
+++ core/src/com/feiqn/wyrm/models/mapdata/prefabLogicalMaps/stage_1a.java
@@ -1,4 +1,21 @@
 package com.feiqn.wyrm.models.mapdata.prefabLogicalMaps;
 
-public class stage_1a {
+import com.badlogic.gdx.utils.Array;
+import com.feiqn.wyrm.WYRMGame;
+import com.feiqn.wyrm.models.mapdata.WyrMap;
+import com.feiqn.wyrm.models.mapdata.tiledata.LogicalTile;
+
+public class stage_1a extends WyrMap {
+
+    public stage_1a(WYRMGame game) {
+        super(game, 50);
+        setUpLogicalTiles();
+    }
+
+    private void setUpLogicalTiles() {
+        final Array<LogicalTile> roadTiles = new Array<>();
+
+
+    }
+
 }
