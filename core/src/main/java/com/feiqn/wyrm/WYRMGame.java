ebc071d9c4cf78b905e93d3f60ee351f18839fee
Qn <feiqn.k@gmail.com>
2024-04-07 09:51:16 -0700

    break

---
+++ core/src/main/java/com/feiqn/wyrm/WYRMGame.java
@@ -0,0 +1,36 @@
+package com.feiqn.wyrm;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.feiqn.wyrm.logic.handlers.CampaignHandler;
+import com.feiqn.wyrm.logic.handlers.WYRMAssetHandler;
+import com.feiqn.wyrm.logic.screens.BattleScreen;
+import com.feiqn.wyrm.logic.screens.MainMenuScreen;
+
+public class WYRMGame extends Game {
+	SpriteBatch batch;
+
+	public ScreenAdapter activeScreen;
+	public BattleScreen activeBattleScreen;
+
+	public WYRMAssetHandler assetHandler;
+	public CampaignHandler campaignHandler;
+
+
+	// Entrance to the program.
+
+	@Override
+	public void create () {
+		assetHandler = new WYRMAssetHandler(this);
+		campaignHandler = new CampaignHandler(this);
+		batch = new SpriteBatch();
+		activeScreen = new MainMenuScreen(this);
+		setScreen(activeScreen);
+	}
+
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
