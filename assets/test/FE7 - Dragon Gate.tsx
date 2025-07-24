4a8a7f6983689f37fceeac3f495f815c1fbcddeb
jessie <feiqn.k@gmail.com>
2025-07-23 21:02:39 -0700

    maps

---
+++ assets/test/FE7 - Dragon Gate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="FE7 - Dragon Gate" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="ripped/fe/FE7 - Dragon Gate - c100c2c3.png" width="512" height="512"/>
+</tileset>
